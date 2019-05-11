dfa8e6b7720b6cfbba9cb38f54765437a8675687
Stanislav Kaisin <8050361@gmail.com>
2019-05-11 21:36:48 +0300

    start eslint

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> eine gute component </h1>{' '}
+      </header>{' '}
+    </div>
+  );
+}
+
+export default App;
